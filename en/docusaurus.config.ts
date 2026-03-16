63bdb1b550bccd5b4ffb7fe4060734759df259de
Samith Karunathilake <samithkarunathilake@gmail.com>
2026-03-16 10:45:09 +0530

    Revert "Add github pages deployment"

--- en/docusaurus.config.ts
+++ en/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     v4: true,
   },
 
-  url: 'https://dev.integrator.docs.wso2.com',
+  url: 'https://integrator.docs.wso2.com',
   baseUrl: '/',
 
   organizationName: 'wso2',
